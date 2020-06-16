f82fa6e8fdf037b964660e358dcef8ab6405f0a5
Chris Tsantiris <chris.tsantiris@gmail.com>
2020-06-16 09:59:12 -0400

    add post route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -15,8 +15,11 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        return 'Hello Task Master'
+    else:
+        return render_template('index.html')
 if __name__ == "__main__":
     app.run(debug=True)
